c0739760dd3486f8a4458e1bdd63060e2b499c52
Spyrius <61468175+Lubo77@users.noreply.github.com>
2023-01-25 09:49:37 +0100

    data fetching update

--- src/App.js
+++ src/App.js
@@ -3,40 +3,57 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const [postsData, setPostsData] = React.useState([])
-  const [usersData, setUsersData] = React.useState([])
-  const [commentsData, setCommentsData] = React.useState([])
+  // const [postsData, setPostsData] = React.useState([])
+  // const [usersData, setUsersData] = React.useState([])
+  // const [commentsData, setCommentsData] = React.useState([])
+  const [data, setData] = React.useState([])
+
+  let postsData = fetch('https://jsonplaceholder.typicode.com/posts')
+  let usersData = fetch('https://jsonplaceholder.typicode.com/users')
+  let commentsData = fetch('https://jsonplaceholder.typicode.com/comments')
 
   React.useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => setPostsData(data))
+    Promise.all([postsData, usersData, commentsData])
+    .then( files => {
+      files.forEach(file => {
+        process(file.json()) //Aj keď fetch prebehne tak tu to vyhodí error. Asi kvôli viacnásobnému volaniu .then .catch
+      })
+    })
+    .catch(() => {
+      console.log('Data fetching failed.')
+    })
   },[])
 
-  React.useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(res => res.json())
-      .then(data => setUsersData(data))
-  },[])
+  const process = (prom) => {
+    prom.then(f => {
+      console.log(f)
+    })
+  }
 
-  React.useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/comments')
-      .then(res => res.json())
-      .then(data => setCommentsData(data))
-  },[])
+  // React.useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then(res => res.json())
+  //     .then(data => setUsersData(data))
+  // },[])
 
-  const posts = postsData.map(item => {
-    return (
-      <Post
-        item={item}
-      />
-    )
-  })
+  // React.useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/comments')
+  //     .then(res => res.json())
+  //     .then(data => setCommentsData(data))
+  // },[])
+
+  // const posts = postsData.map(item => {
+  //   return (
+  //     <Post
+  //       item={item}
+  //     />
+  //   )
+  // })
 
   return (
     <div className='app-body'>
       <div>
-        {posts}
+        {/* {posts} */}
       </div>
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Post(props) {
 
+    
+
     return (
         <div className='post-body'>
             <h1>{props.item.title}</h1>
