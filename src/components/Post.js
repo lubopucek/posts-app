5a90bc0cec0dabe364190604adcb13e3fa16617e
Spyrius <61468175+Lubo77@users.noreply.github.com>
2023-07-28 15:16:35 +0200

    styles update

--- src/components/Post.js
+++ src/components/Post.js
@@ -8,8 +8,8 @@ function Post(props) {
         <h5 className='post__title'>{props.item.title}</h5>
         <h6 className='post__author'>{props.user.name}</h6>
         <p className='post__body'>{props.item.body}</p>
-        <div>
-          <span>Show more...</span>
+        <div className='post__container'>
+          <span className='post__showmore'>Show more...</span>
           <span className='post__comment'>Comments({props.numOfComments})</span>
         </div>
       </div>
